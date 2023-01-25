d2fffa5a52e1bd6423a11303d58e449a12072ca3
Hugo Goorickx <hugo@goorickx.be>
2023-01-25 14:57:41 +0100

    Ajout de la page contact

--- contact_me.php
+++ contact_me.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/index.css">
     <link rel="stylesheet" href="./assets/contact_me.css">
     <title>Contact me</title>
 </head>
@@ -15,70 +16,67 @@
             return str_replace(["'", '"'], ['&#39;', '&#34;'], $str);
         }
 
-            if (isset($_POST["name"]) && isset($_POST["desc"]) && isset($_POST["mail"]))
+        if (isset($_POST["name"])   && isset($_POST["desc"])    && isset($_POST["mail"]) &&
+            strlen($_POST["name"])  && strlen($_POST["desc"])   && strlen($_POST["mail"]))
+        {
+            $name = filter_string_polyfill($_POST["name"]);
+            $mail = filter_var(filter_string_polyfill($_POST["mail"]), FILTER_SANITIZE_EMAIL);
+            if (!filter_var($mail,FILTER_VALIDATE_EMAIL))
             {
-                $name = filter_string_polyfill($_POST["name"]);
-                $mail = filter_string_polyfill($_POST["mail"]);
-                $mail = filter_var($mail, FILTER_SANITIZE_EMAIL);
-                if (!filter_var($mail,FILTER_VALIDATE_EMAIL))
+                echo "nope1";
+                return;
+            }
+            
+            if (isset($_POST["phone"]) && strlen($_POST["phone"]))
+            {
+                $phone = filter_string_polyfill($_POST["phone"]);
+                if(!preg_match('/^[0-9]{10}+$/', $phone))
                 {
-                    echo "nope1";
+                    echo "nope2";
                     return;
                 }
-                if (isset($_POST["phone"]))
-                {
-                    $phone = filter_string_polyfill($_POST["phone"]);
-                    if(!preg_match('/^[0-9]{10}+$/', $phone))
-                    {
-                        echo "nope2";
-                        return;
-                    }
-                }
-                $desc = filter_string_polyfill($_POST["desc"]);
-
-
-                $string = "INSERT INTO contact (Nom, Mail, Phone, Detail, Date_message) VALUES ('" . $name . "','" . $mail . "','" . $phone . "','" . $desc . "', 1);";
-                echo $string;
-
-                try
-                {
-                    $bdd = new PDO("mysql:host=hugoorickx.be;dbname=u716273791_me", "u716273791_hugoorickx", "8X=HB]mW&px");
-                    // set the PDO error mode to exception
-                    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    $query = $bdd->query($string);
-                    $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
-                    echo "Connected successfully"; 
-                }
-                catch(PDOException $e)
-                {
-                    echo "Connection failed: " . $e->getMessage();
-                }
-                echo "ici";
-                
-                // $to = 'hugoorickx@gmail.com';
-                // $subject = 'Objet du mail';
-                // $message = 'Contenu du mail';
-                // $headers = 'From: '.$mail.'\'' . "\r\n" .
-                //     'Reply-To: '.$mail.'\'' . "\r\n" .
-                //     'X-Mailer: PHP/' . phpversion();
-
-                // if (!mail($to, $subject, $message, $headers)) {
-                //     echo error_get_last()['message'];
-                // }
             }
-            // else
+
+            $desc = filter_string_polyfill($_POST["desc"]);
+
+            date_default_timezone_set('UTC');
+            $string = "INSERT INTO contact (Nom, Mail, Phone, Detail, Date_message) VALUES ('" . $name . "','" . $mail . "','" . $phone . "','" . $desc . "', '".date('j F Y h:i:s')."');";
+            echo $string;
+
+            // try
             // {
-            //     {
-            //         echo "nope3";
-            //         return;
-            //     }
+            //     $bdd = new PDO("mysql:host=localhost;dbname=u716273791_me", "u716273791_hugoorickx", "8X=HB]mW&px");
+            //     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            //     $query = $bdd->query($string);
+            //     $resultat = $query->fetchAll(PDO::FETCH_ASSOC);
+            //     echo "Connected successfully"; 
             // }
+            // catch(PDOException $e)
+            // {
+            //     echo "Connection failed: " . $e->getMessage();
+            // }
+            
+            // $to = 'hugoorickx@gmail.com';
+            // $subject = 'Objet du mail';
+            // $message = 'Contenu du mail';
+            // $headers = 'From: '.$mail.'\'' . "\r\n" .
+            //     'Reply-To: '.$mail.'\'' . "\r\n" .
+            //     'X-Mailer: PHP/' . phpversion();
+
+            // if (!mail($to, $subject, $message, $headers)) {
+            //     echo error_get_last()['message'];
+            // }
+        }
     ?>
     <header>
-        <div class="github"></div>
-        <div class="tel"></div>
-        <div class="mail"></div>
-    </header>
+            <nav>
+                <div class="links-content up">
+                        <a class="links-content__item" href="index.html#blocHorizontal"> Projects </a>
+                        <a class="links-content__item" href="index.html#"> About me </a>
+                        <a class="links-content__item" href="index.html#contact"> Links </a>
+                </div>
+            </nav>
+        </header>
     <main>
         <form action="./contact_me.php" method="post">
             <div class="input-container">
